9112b8e5b879696f898243dba4e5f276bca839d7
adambmyers <blase_adam@yahoo.com>
2019-03-17 20:52:00 -0400

    finalized portfolio 2

--- js/background.js
+++ js/background.js
@@ -1,25 +1,73 @@
-// Get the modal
-var modal = document.getElementById('modal-resort');
+// start header nav
+run();
 
-// Get the button that opens the modal
-var btn = document.getElementById("GWbutton");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function run() {
+  bindNavButtonToNavList();
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function toggleHide(element) {
+  element.classList.toggle('hide');
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function bindNavButtonToNavList() {
+  const navButton = document.querySelector('.nav__button');
+  
+  navButton.addEventListener('click', function() {
+    const navList = document.querySelector('.nav__list');
+    toggleHide(navList);
+  });
 }
+// end header nav
+
+var GWmodal = document.querySelector(".modal-display-box-GW");
+var GWtrigger = document.querySelector(".GWbutton");
+var CBmodal = document.querySelector(".modal-display-box-CB");
+var CBtrigger = document.querySelector(".CBbutton");
+var KRmodal = document.querySelector(".modal-display-box-KR");
+var KRtrigger = document.querySelector(".KRbutton");
+var MSmodal = document.querySelector(".modal-display-box-MS");
+var MStrigger = document.querySelector(".MSbutton");
+var closeButton = document.querySelector(".close-button");
+const modalDisplayBoxGw = document.querySelector('.modal-display-box-GW')
+const modalDisplayBoxCb = document.querySelector('.modal-display-box-CB')
+const modalDisplayBoxKr = document.querySelector('.modal-display-box-KR')
+const modalDisplayBoxMs = document.querySelector('.modal-display-box-MS')
+
+function toggleModalGW() {
+    GWmodal.classList.toggle("show-GWmodal");
+}
+function toggleModalCB() { 
+    CBmodal.classList.toggle("show-CBmodal");
+}   
+function toggleModalKR() {
+    KRmodal.classList.toggle("show-KRmodal");
+}
+function toggleModalMS() {
+    MSmodal.classList.toggle("show-MSmodal");
+}
+
+function windowOnClick(event) {
+    if (event.target === GWmodal) {
+       toggleModalGW()
+    } else if 
+        (event.target === CBmodal) {
+        toggleModalCB();
+    } else if
+        (event.target === KRmodal) {
+        toggleModalKR();
+    } else {
+        toggleModalMS();    
+    }   
+    }
+
+GWtrigger.addEventListener("click", toggleModalGW);
+modalDisplayBoxGw.addEventListener('click', toggleModalGW)
+
+CBtrigger.addEventListener("click", toggleModalCB);
+modalDisplayBoxCb.addEventListener('click', toggleModalCB)
+
+KRtrigger.addEventListener("click", toggleModalKR);
+modalDisplayBoxKr.addEventListener('click', toggleModalKR)
+
+MStrigger.addEventListener("click", toggleModalMS);
+modalDisplayBoxMs.addEventListener('click', toggleModalMS)
